d197b29464652aaa07fc30b77dd497bebbb9be94
Justen Seward <JA_Seward@hotmail.com>
2022-04-28 14:29:03 -0500

    removed printmenu from printboard

--- printboard.py
+++ printboard.py
@@ -8,13 +8,3 @@ def printboard(board_list): # Prints board in easy to read layout.
     print('')
     print('─'*24)
     x+=1
-  
-def printmenu():
-  print('MAIN MENU')
-  print('Enter number for option')
-  print('1 - New Game')
-  print('2 - Join Existing Game')
-  print('3 - Get Board')
-  print('4 - Get Game Status')
-  print('5 - Get Next Ball')
-  print('6 - Quit Game & Client')
