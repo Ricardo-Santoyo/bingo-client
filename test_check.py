4eae81195c8517fca1381242afb6821748905e38
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-04-28 15:39:34 -0500

    updated unit tests in test_check.py file

--- test_check.py
+++ test_check.py
@@ -27,17 +27,17 @@ class TestCheckColumns(unittest.TestCase): # Tests for bingo in columns.
 
   def test_check_for_bingo_success_1(self): # Tests B column.
     actual = check.check_columns(make_board(0, True))
-    expected = True
+    expected = "C1"
     self.assertEqual(actual, expected)
 
   def test_check_for_bingo_success_2(self): # Tests N column.
     actual = check.check_columns(make_board(2, True))
-    expected = True
+    expected = "C3"
     self.assertEqual(actual, expected)
 
   def test_check_for_bingo_success_3(self): # Tests O column.
     actual = check.check_columns(make_board(4, True))
-    expected = True
+    expected = "C5"
     self.assertEqual(actual, expected)
 
 class TestCheckDiagonals(unittest.TestCase): # Tests for bingo in diagonals.
@@ -51,7 +51,7 @@ class TestCheckDiagonals(unittest.TestCase): # Tests for bingo in diagonals.
     for i in range(5):
       new_board[i][i] = 'x'
     actual = check.check_diagonals(new_board)
-    expected = True
+    expected = "DL"
     self.assertEqual(actual, expected)
 
   def test_check_for_bingo_success_2(self): # Tests diagonal running bottom to top.
@@ -59,7 +59,7 @@ class TestCheckDiagonals(unittest.TestCase): # Tests for bingo in diagonals.
     for i in range(5):
       new_board[i][4 - i] = 'x'
     actual = check.check_diagonals(new_board)
-    expected = True
+    expected = "DR"
     self.assertEqual(actual, expected)
 
 class TestCheckRows(unittest.TestCase): # Tests for bingo in rows.
@@ -70,15 +70,15 @@ class TestCheckRows(unittest.TestCase): # Tests for bingo in rows.
   
   def test_check_for_bingo_success_1(self): # Tests top row.
     actual = check.check_rows(make_board(0))
-    expected = True
+    expected = "R1"
     self.assertEqual(actual, expected)
 
   def test_check_for_bingo_success_2(self): # Tests middle row.
     actual = check.check_rows(make_board(2))
-    expected = True
+    expected = "R3"
     self.assertEqual(actual, expected)
 
   def test_check_for_bingo_success_3(self): # Tests bottom row.
     actual = check.check_rows(make_board(4))
-    expected = True
+    expected = "R5"
     self.assertEqual(actual, expected)
